d3fae71a4571fb76b0e54bdeb255b69dbea316a2
DoriPil <dorian.pillard@minesparis.psl.eu>
2025-12-11 17:14:44 +0100

    Update run_all_benchmarks.py

--- experiments/run_all_benchmarks.py
+++ experiments/run_all_benchmarks.py
@@ -1,9 +1,26 @@
 import subprocess
 import sys
 
-subprocess.run([sys.executable, r"C:\Users\PILLLARD-DOR\Documents\TEMPO\methods\stack_method\method.py"])
-subprocess.run([sys.executable, r"C:\Users\PILLLARD-DOR\Documents\TEMPO\methods\compound_sharp_method\method.py"])
-subprocess.run([sys.executable, r"C:\Users\PILLLARD-DOR\Documents\TEMPO\methods\depth_map_method\method.py"])
-subprocess.run([sys.executable, r"C:\Users\PILLLARD-DOR\Documents\TEMPO\methods\mono_slice_method\method.py"])
+# Passer en argument "--dataPath" le chemin vers le dossier des données de travail, et en script le chemin vers le script à exécuter
+
+subprocess.run([sys.executable, 
+                r"C:\Users\PILLLARD-DOR\Documents\GitHub\TEMPO\methods\stack_method\method.py",
+                "--dataPath",r"C:\Users\PILLLARD-DOR\Documents\TEMPO\data",
+                "--resultsPath",r"C:\Users\PILLLARD-DOR\Documents\TEMPO\results_and_intermediate"])
+
+subprocess.run([sys.executable,
+                r"C:\Users\PILLLARD-DOR\Documents\GitHub\TEMPO\methods\compound_sharp_method\method.py",
+                "--dataPath",r"C:\Users\PILLLARD-DOR\Documents\TEMPO\data",
+                "--resultsPath",r"C:\Users\PILLLARD-DOR\Documents\TEMPO\results_and_intermediate"])
+
+subprocess.run([sys.executable, 
+                r"C:\Users\PILLLARD-DOR\Documents\GitHub\TEMPO\methods\depth_map_method\method.py",
+                "--dataPath",r"C:\Users\PILLLARD-DOR\Documents\TEMPO\data",
+                "--resultsPath",r"C:\Users\PILLLARD-DOR\Documents\TEMPO\results_and_intermediate"])
+
+subprocess.run([sys.executable, 
+                r"C:\Users\PILLLARD-DOR\Documents\GitHub\TEMPO\methods\mono_slice_method\method.py",
+                "--dataPath",r"C:\Users\PILLLARD-DOR\Documents\TEMPO\data",
+                "--resultsPath",r"C:\Users\PILLLARD-DOR\Documents\TEMPO\results_and_intermediate"])
 # Module pour faire fonctionner SAM pas encore installé sur la machine donc pas encore de script method.py associé, les images et masques obtenus sont tirés de l'outil en ligne pour le moment
-#subprocess.run([sys.executable, r"C:\Users\PILLLARD-DOR\Documents\TEMPO\methods\SAM_method\method.py"])
+#subprocess.run([sys.executable, r"C:\Users\PILLLARD-DOR\Documents\TEMPO\methods\SAM_method\method.py"])"""
